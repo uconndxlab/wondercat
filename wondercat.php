bea10e42a97f4515cdb8fe36943d9b3e362b2fc4
Brian Daley <brian@uconn.edu>
2024-12-07 20:56:20 -0500

    cleanup and git updater config

--- wondercat.php
+++ wondercat.php
@@ -8,64 +8,24 @@
  *
  * @wordpress-plugin
  * Plugin Name:   WonderCat
- * Plugin URI:    https://mydomain.com
+ * Plugin URI:    https://github.com/uconndxlab/wondercat
  * Description:   Allows clients to download WonderCat data through REST endpoints.
  * Version:       1.0.0
  * Author:        Brian Daley
  * Author URI:    https://dxgroup.core.uconn.edu/
  * Text Domain:   wondercat
  * Domain Path:   /languages
+ * GitHub Plugin URI: uconndxlab/wondercat
+ * GitHub Plugin URI: https://github.com/uconndxlab/wondercat
+ * Primary Branch: main
  */
 
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-// function my_acf_form_submission_handler($form_name, $form_data)
-// {
-//     log_it('plugin running');
-//     // // Check if the submitted form matches your desired form name (e.g., "my-form")
-//     // if ($form_name === 'my-form') {
-//     //     // Get the form data
-//     //     $data = array();
-//     //     foreach ($form_data as $field => $value) {
-//     //         $data[$field] = $value;
-//     //     }
-
-//     //     // Use the form data to query another API (e.g., using cURL or a library like Guzzle)
-//     //     $api_url = 'https://example.com/api/endpoint';
-//     //     $response = wp_remote_get($api_url, array('query' => $data));
-
-//     //     if (!is_wp_error($response)) {
-//     //         // Process the response data
-//     //         $result_data = json_decode($response['body'], true);
-//     //         // Do something with the result data (e.g., store it in a database or display it on a page)
-//     //     }
-//     // }
-//     log_it($form_name);
-
-//     // do_action('qm/debug', [$form_name, $form_data]);
-// }
-
-// Register the ACF form submission handler
-// add_action('acf_form_submit', 'my_acf_form_submission_handler');
-
-
-// function my_acf_update_value_handler($field, $value)
-// {
-//     // // Alter the field value as needed (e.g., trim whitespace)
-//     // if ($field->name === 'my_field') { // Replace with your desired field name
-//     //     $value = trim($value);
-//     // }
-
-//     // return $value;
-//     do_action('qm/debug', $field, $value);
-
-// }
-
 $qid_key = 'field_66ec85b50b8f1';
 $wikidata_key = 'field_67528b4e4f253';
 
-// add_filter('acf/update_value', 'my_acf_update_value_handler');
 if (!function_exists('log_it')) {
     function log_it($message)
     {
@@ -80,7 +40,7 @@ if (!function_exists('log_it')) {
 }
 
 add_action('acf/save_post', 'my_acf_save_post');
-function my_acf_save_post($post_id)
+function my_acf_save_post($post_id): void
 {
     global $qid_key, $wikidata_key;
     log_it($post_id);
